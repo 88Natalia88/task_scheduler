043820f86ad7d3bb711b9f841ece89f5ae3f1afb
Natalia Balashova <88karamelka88@mail.ru>
2023-03-10 21:02:41 +0300

    work

---
+++ js/index1.js
@@ -0,0 +1,10 @@
+let nums = document.getElementById('num');
+let btn = document.getElementById('btn');
+let text = document.getElementById('text');
+
+btn.addEventListener('click', function(){
+let num = nums.value;
+let numbers = [];
+numbers.push(+num);
+
+});
