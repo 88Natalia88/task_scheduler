d0cb58611480c14697900a143b41247e8d0c1a36
Natalia Balashova <88karamelka88@mail.ru>
2023-03-11 12:01:57 +0300

    work

--- js/index1.js
+++ js/index1.js
@@ -8,10 +8,14 @@ const around = document.getElementById('around');
 btn.addEventListener('click', function(){
 let num = nums.value;
 let array = Array.from(num);
+for(let arr of array){
+    let sum = 0;
+    sum = arr + arr;
+}
 text.innerHTML = `Введенное число состоит из цифр: ${String(array)}`;
 //example.innerHTML = `Сумма введенных цифр: ${sum}`;
 around.innerHTML = `Введенное значение наоборот: ${array.reverse()}`;
 long.innerHTML = `Длина введенного значения: ${array.length} цифры`;
 //console.log(array.length);
-
+example.innerHTML = `Сумма введенных цифр: ${sum}`;
 });
