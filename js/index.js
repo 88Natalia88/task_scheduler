aeb4f00859daf7b7f7412b93899620159ca71c5f
Natalia Balashova <88karamelka88@mail.ru>
2023-03-12 15:14:37 +0300

    js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,27 @@
 const task = document.getElementById('task');
 const text = document.getElementById('text');
 const btn = document.getElementById('btn');
-
+const btn1 = document.getElementById('btn1');
 
 btn.addEventListener('click', function(){
     let taskEnd = task.value;
-    text.innerHTML += `<p>${taskEnd}</p>
-    <input type="checkbox" id="yes">`;
+    if(taskEnd === ''){
+        text.innerHTML = 'Нет задач';
+        text.style.cssText = "border: none";
+    } else {
+        text.textContent = '';
+        const textNew = document.createElement('div');
+        textNew.className = 'tasks';
+        textNew.innerHTML += `<p>${taskEnd}</p>
+        <input type="checkbox" id="yes">`;
+        text.append(textNew);
+        textNew.style.cssText = " display: flex; justify-content: space-around; color: black";
+        text.style.cssText = "border-bottom: 2px solid rgb(197, 179, 179); margin-bottom: 16px;";
+    }
+   
 });
+btn1.addEventListener('click' , clean);
+function clean(){
+    text.innerHTML = 'Нет задач';
+    text.style.cssText = "border: none";
+}
